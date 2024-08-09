4e2a1e07bb90cbacc4b7b00024c74453d2a820c6
Mikhail-Mukha <misha500krut@gmail.com>
2024-08-09 19:20:01 +0300

    finaly-commit

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -9,7 +9,7 @@ export const initialState = {
   },
 };
 import { createSlice } from "@reduxjs/toolkit";
-import { fetchContacts, addContact } from "./contactsOps";
+import { fetchContacts, addContact, deleteContact } from "./contactsOps";
 
 const slice = createSlice({
   name: "contacts",
@@ -21,10 +21,14 @@ const slice = createSlice({
       })
       .addCase(addContact.fulfilled, (state, action) => {
         state.items.push(action.payload);
+      })
+      .addCase(deleteContact.fulfilled, (state, action) => {
+        state.items = state.items.filter(
+          (contact) => contact.id !== action.payload
+        );
       });
   },
 });
 
 export const contactsReducer = slice.reducer;
-export const { deleteContact } = slice.actions;
 export const selectContacts = (state) => state.contacts.items;
